46100c18e32ae2bd7262ee6eadc47248d16f60e5
huulamnguyen <liamnguyen.swe@gmail.com>
2021-12-14 11:33:57 -0500

    commit tree c3f845ac7ad66e008b7f9adabf3da53cefd4f0a1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function saturdayFun(act = 'roller-skate'){
+    return `This Saturday, I want to ${act}!`
+};
+
+const mondayWork = function (act = "go to the office"){
+    return `This Monday, I will ${act}.`
+};
